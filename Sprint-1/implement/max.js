45c43637452f30f7ee7477800b328fc29ea2bf3d
JuneMonton <junemonton@JuneMontons-MacBook-Air.local>
2026-02-14 10:19:56 +0800

    Added dedupe and max functions with tests

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,21 @@
 function findMax(elements) {
+  // Start with -Infinity (smaller than any number)
+  let max = -Infinity;
+  
+  // Look at each element in the array
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    
+    // Check if it's actually a number
+    if (typeof element === 'number') {
+      // If this number is bigger than our current max, update max
+      if (element > max) {
+        max = element;
+      }
+    }
+  }
+  
+  return max;
 }
 
 module.exports = findMax;
