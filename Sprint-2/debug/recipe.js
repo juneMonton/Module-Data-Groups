4f62e6274f9a17a6544d06e4c3440ae55e92fc4f
JuneMonton <junemonton@JuneMontons-MacBook-Air.local>
2026-02-14 13:27:40 +0800

    Modified address,author and recipe functions

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -4,6 +4,8 @@
 // Each ingredient should be logged on a new line
 // How can you fix it?
 
+
+/*
 const recipe = {
   title: "bruschetta",
   serves: 2,
@@ -13,3 +15,23 @@ const recipe = {
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
 ${recipe}`);
+
+This code will not work as intended. The issue is ${recipe} at the end - you're trying to interpolate the entire object, which will just print [object Object], not the ingredients array.
+You need to access recipe.ingredients and either:
+
+Loop through the array
+Use .join('\n') to create newline-separated strings
+
+*/
+
+
+
+const recipe = {
+  title: "bruschetta",
+  serves: 2,
+  ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
+};
+
+console.log(`${recipe.title} serves ${recipe.serves}
+  ingredients:
+${recipe.ingredients.join('\n')}`);
