da95184148d75955226e2c66914a4af8abd9367b
junemonton <junemonton.dev@gmail.com>
2026-04-05 10:40:43 +0200

    Added changes to the js and html file

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,29 @@
-function setAlarm() {}
+function setAlarm() {
+  var totalSeconds = Number(document.getElementById("alarmSet").value);
+
+  function updateDisplay(seconds) {
+    var minutes = Math.floor(seconds / 60);
+    var remainingSeconds = seconds % 60;
+    var display =
+      String(minutes).padStart(2, "0") +
+      ":" +
+      String(remainingSeconds).padStart(2, "0");
+    document.getElementById("timeRemaining").innerText =
+      "Time Remaining: " + display;
+  }
+
+  updateDisplay(totalSeconds);
+
+  var timer = setInterval(function () {
+    totalSeconds = totalSeconds - 1;
+    updateDisplay(totalSeconds);
+
+    if (totalSeconds === 0) {
+      clearInterval(timer);
+      playAlarm();
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
